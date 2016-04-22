0e72d925673316e39839a38e56b2c51098294327
dutxueyu <leop22@163.com>
2016-04-22 09:37:43 +0800

    new project
    userControl to make new slide

---
+++ slideNN/WpfApplication4/sliderBlue.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication4
+{
+    /// <summary>
+    /// sliderBlue.xaml 的交互逻辑
+    /// </summary>
+    public partial class sliderBlue : UserControl
+    {
+        public static double slideWid;
+        public sliderBlue()
+        {
+            InitializeComponent();
+            slideWid = slideNN.Width;
+        }
+
+    }
+    public class valuetoLeft : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //RelativeSource rs = new RelativeSource(RelativeSourceMode.FindAncestor);
+            //rs.AncestorType = typeof(Grid);
+            ;
+            double len = (double)value;
+            len = 47 * len;
+            return len;
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+    public class CanVisiConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value == false)
+                return Visibility.Hidden;
+            else
+                return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    public class textAddConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double  text = (double)value;
+            string txtadd = 10*text + "%";
+            return txtadd;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
